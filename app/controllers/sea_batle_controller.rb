b21e87c996ce5d6ec1cf499958cf2d26d0dba6c1
Mikhail Melnyk <m1kh41l.melnyk@gmail.com>
2018-11-21 19:14:38 +0200

    some token check

--- app/controllers/sea_batle_controller.rb
+++ app/controllers/sea_batle_controller.rb
@@ -1,19 +1,38 @@
 class SeaBatleController < ApplicationController
-    def single
-        params.permit!
-        if params[:code]
-          render json: params, status: 200
-        else
-          render json: {}, status: 422
-        end
-    end
+  def single
+    params.permit!
+    render json: {}, status: 422 unless params[:code]
+    render json: params, status: 200
+  end
 
-    def multi
-        params.permit!
-        if params[:first_code] && params[:second_code]
-          render json: params, status: 200
-        else
-          render json: {}, status: 422
-        end
+  def multi_generate
+    params.permit!
+    render json: {}, status: 422 unless params[:code]
+    hash = rand(2**256).to_s(36).ljust(8,'a')[0..15]
+    open("app/code/"+hash+"_1.code", "w") do |f|
+      f.puts params[:code]
     end
+    Multi.create(:token => hash)
+    render json: hash, status: 200
+  end
+
+  def multi_accept
+    params.permit!
+    render status:422 unless params[:token]
+    render status:422 unless params[:code]
+    render status:422 unless Multi.where(:token => params[:token]).any?
+    open("app/code/"+params[:token]+"_2.code", "w") do |f|
+      f.puts params[:code]
+    end
+    Multi.where(:token => params[:token]).first.delete
+    system("cd app/code && rm "+params[:token]+"_1.code"+" && rm "+params[:token]+"_2.code")
+    ans = war(params[:token])
+    render json: ans , status: 200
+  end
+
+  private
+
+  def war(token)
+    return 1
+  end
 end
